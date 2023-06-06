57468b31c8b1462a2b2464e04a4816702b1dca45
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-06-06 16:28:40 -0700

    list

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = rendom.randint(-10, 10)
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is ero".format(number))
+else:
+    print("{} is negative".format(number))
