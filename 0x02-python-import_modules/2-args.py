1f7429010bc7dec2378fbdabfa9643dd728d396a
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-06-08 16:12:39 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv[1:])
+    if num == 0:
+        print("0arguments.")
+    elif num == 0:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num))
+    for i in range(num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
