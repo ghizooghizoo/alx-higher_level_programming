d21f3533082866b2fe1136f26e62683f95545bfb
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-06-14 16:06:55 -0700

    data

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq = set(my_list)
+    n = 0
+    for i in uniq:
+        n += i
+    return (n)
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    n = 0
+    listkeys = list(a_dictionary.keys())
+    for i in listkeys:
+        n += 1
+    return (n)
