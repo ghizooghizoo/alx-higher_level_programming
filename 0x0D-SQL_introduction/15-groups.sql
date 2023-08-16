c19a4aac8a3ffab28833d4ccf2a0f79726d24d98
ghizooghizoo <benmoumenghizlane55@gmil.com>
2023-08-16 16:08:46 -0700

    inntroduction

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
-SELECT score, COUNT(1) AS number FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+-- lists the number of records with the same score in the table second_table
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score` ORDER BY `number` DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
-SELECT score, name
-FROM second_table
-HAVING name IS NOT NULL
-ORDER BY score DESC;
+-- lists all records of the table second_table
+-- Don’t list rows without a name value
+SELECT `score`, `name` FROM `second_table`
+WHERE `name` != "" ORDER BY `score` DESC;
