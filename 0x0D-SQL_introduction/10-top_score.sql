1df3e34491d5bb9a42dddc10ddba6a6a487e93d7
ghizooghizoo <benmoumenghizlane55@gmil.com>
2023-08-16 15:52:29 -0700

    introduction

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,5 @@
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- lists all records of the table second_table
+-- Results should display both the score and the name (in this order)
+-- according to high scores 
+SELECT `score`, `name` FROM `second_table`
+ORDER BY `score` DESC;
