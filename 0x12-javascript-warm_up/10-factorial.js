1d96dfaf6b8fe9b5198350ad56bd0db1f2b71213
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-09-11 16:22:13 -0700

    java

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-function factorial (a) {
-  let sum = 1;
-  if (isNaN(a) || a === 1) {
-    return (1);
-  }
-  for (let i = a; i > 0; i--) {
-    sum *= i;
-  }
-  return (sum);
+
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
-console.log(factorial(parseInt(process.argv[2])));
+
+console.log(factorial(Number(process.argv[2])));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
   console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
-const elements = process.argv.sort();
-console.log(elements[elements.length - 2]);
